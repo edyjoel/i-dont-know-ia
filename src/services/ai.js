82b29daec8c3f8b17bc168c9777aeb1b92fc3a53
Edy <edyxicon@gmail.com>
2023-02-06 23:32:20 -0600

    Update return

--- src/services/ai.js
+++ src/services/ai.js
@@ -23,23 +23,5 @@ export async function getAnswer(answer) {
     }),
   });
   const data = await response.json();
-  console.log(data);
-  // return data.choices[0].text;
+  return data.generations[0].text;
 }
-
-// curl --location --request POST 'https://api.cohere.ai/generate' \
-//   --header 'Authorization: BEARER X6ahuAaFZcTOfV2F4b9l2d0N7NvWBl3z5aW7AAkn' \
-//   --header 'Content-Type: application/json' \
-//   --header 'Cohere-Version: 2022-12-06' \
-//   --data-raw '{
-//       "model": "command-xlarge-nightly",
-//       "prompt": "Generate paragraph with the answer to this question \"What is you range salary?\"",
-//       "max_tokens": 300,
-//       "temperature": 1.1,
-//       "k": 0,
-//       "p": 0.75,
-//       "frequency_penalty": 0,
-//       "presence_penalty": 0,
-//       "stop_sequences": [],
-//       "return_likelihoods": "NONE"
-//     }'
